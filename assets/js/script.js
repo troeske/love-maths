67c3b58ab14e43215ce928a584cd9bb46c75ddf0
Thorsten Roeske <thorsten@wearonize.com>
2024-07-12 10:08:48 +0000

    add eventhandlers for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+/* wait for DOM to finish loading */
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons){
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }    
+        })    
+    }
+})
+
 function runGame() {
 
 }
