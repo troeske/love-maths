ae35739c354420ee123ed74b4088f158832f22ae
Thorsten Roeske <thorsten@wearonize.com>
2024-07-12 11:35:42 +0000

    add js code for addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,29 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked submit");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }    
         })    
     }
+
+    runGame("addition");
 })
 
 /** 
  * hte main game 'loop' called when script is first loaded
  * and after the user's answer has been processed
 */
-function runGame() {
+function runGame(gameType) {
     /* randoms numbers */
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType} - Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -40,8 +48,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion() {
